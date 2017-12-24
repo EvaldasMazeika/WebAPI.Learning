a6f3c9fb878b0767f9be5b275dbbc448559c2d19
Evaldas Mažeika <evaldass.mazeika@gmail.com>
2017-12-24 16:50:41 +0200

    Re created project + added controller

---
+++ WebApi.TODOList/WebApi.TODOList/Controllers/TodoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.TODOList.Controllers
+{
+    [Route("api/todos")]
+    public class TodoController : Controller
+    {
+
+        [HttpGet()]
+        public IActionResult GetTodos()
+        {
+            return Ok();
+        }
+    }
+}
